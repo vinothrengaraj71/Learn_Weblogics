1a9025b857813dccce8d6ab0a0214edb26f7f40a
Vinoth Rengaraj <vinothrengaraj71@gmail.com>
2024-01-10 12:04:48 -0500

    Array stuffs

--- Scripts/mainscript.js
+++ Scripts/mainscript.js
@@ -1,1 +1,11 @@
 console.log("vinoth");
+const friends = ['vinoth','vindy'];
+
+console.log(friends);
+
+
+console.log(friends[0]);
+
+console.log(friends.length);
+console.log(friends.lastIndexOf());
+
